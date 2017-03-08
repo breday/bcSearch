9ddf6b0a55aea29174b02b108a3e2edf84cb30cb
brenda nyokabi <brndnyokabi@gmail.com>
2017-03-08 12:59:38 +0300

    get names of restaurants from washington

--- search_restaurant.py
+++ search_restaurant.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib
 import json
 
 locu_api = S8c0fdafa35e5ce90c36ede4ee4184bdc40b9f6fe
@@ -6,9 +6,31 @@ url =('https://api.locu.com/v1_0/venue/search/?country=UNITED%20STATES&locality=
 json_obj = urllib.urlopen(url)
 data = json.load(json_obj)
 
+
+
+restaurant = input(Do you want to see all restaurants found around the  washington area?)
 for item in data['objects']:
-	print item(name)
-	print item(locality)
-	print item(country)
+	if restaurant =="yes":
+		return item['name']
+	else:
+		return None
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 
